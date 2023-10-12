8986cb4e548f78006db4c56234275c1b801043b9
C0deepak <106076858+C0deepak@users.noreply.github.com>
2023-10-12 15:06:01 +0530

    Update FaqData.js
    faq 3 deleted

--- src/utils/FaqData.js
+++ src/utils/FaqData.js
@@ -14,21 +14,6 @@ const FaqData = [
         question: 'Give some Details ?',
         answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique nobis deleniti corporis commodi error doloribus rem labore impedit eum nam.'
     },
-    {
-        id: 4,
-        question: 'How i work and what are my designations ?',
-        answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique nobis deleniti corporis commodi error doloribus rem labore impedit eum nam.'
-    },
-    {
-        id: 5,
-        question: 'Love to see my work what after?',
-        answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique nobis deleniti corporis commodi error doloribus rem labore impedit eum nam.'
-    },
-    {
-        id: 6,
-        question: 'Any further plan to collabrate',
-        answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique nobis deleniti corporis commodi error doloribus rem labore impedit eum nam.'
-    },
 ]
 
 export {FaqData};
