f06a5095bedd549546728399c0a32a1339743b87
Avinash Yadav <90188364+AceHere@users.noreply.github.com>
2024-01-04 23:00:43 +0530

    Update Contact.js
    removed text and subscribe section

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -12,8 +12,7 @@ const Contact = () => {
       <div className='contactHero'>
         <div className="contactHeroText">
           <div className="contactHeroHeading">Feel free to <span>Reach me</span></div>
-          <div className="contactHeroPara">I'm here and ready to connect. Feel free to drop me a line with any questions, thoughts, or ideas you have. Whether you're looking to collaborate, share feedback, or simply have a conversation, your message is important to me. I look forward to hearing from you and exploring the possibilities together.</div>
-        </div>
+          </div>
       </div>
 
       <div className='contactAbout'>
@@ -21,9 +20,6 @@ const Contact = () => {
           <div className="contactAboutLeftTop">
             <div className='otherHeading'>Get in <span>Touch</span></div>
             <div className="contactALText">
-              <div className="contactALTPara">
-                We welcome your contact and encourage you to connect with us. Whether you have inquiries, ideas to share, or potential collaborations in mind, we're here and eager to engage. Your message is important to us, and we're excited about the opportunity to connect and explore the possibilities together. Feel free to reach out – we're just a message away!
-              </div>
             </div>
             <a href="https://calendly.com/avinash-yt" className='buttonLink'><YellowButton text="Schedule a call" icon={<FaVideo />} /></a>
             <div className="contactALSocial">
@@ -37,25 +33,7 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className="contactAboutLeftBottom">
-            <div className='otherHeading'><span>Subscribe</span> to my channel</div>
-            <div className="contactALSubscribe">
-              <div className="contactALSDetail">
-                <div className="contactALSDImg">
-                  <img src="/img/avatar.png" alt="" />
-                </div>
-                <div className="contactALSDText">
-                  <div className="contactALSDTHeading">Beyond Avinash</div>
-                  <div className="contactALSDTPara">
-                    <div className="contactALSDTPDiv">@BeyondAvinash</div>
-                    <div className="contactALSDTPDiv">1.21K subscribers</div>
-                  </div>
-                </div>
-              </div>
-              <a href='https://youtube.com/@BeyondAvinash?si=RRHHlS7_uJan2wTC' target='_blank' className="contactALSButton">Subscribe</a>
-            </div>
-          </div>
-        </div>
+          
         <div className="contactAboutRight">
           <form action="https://formspree.io/f/xpzgrwlw"
             method="POST">
