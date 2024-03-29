073cef07eafd569ed1e038fef63e128d15ff63de
Avinash Yadav <90188364+AceHere@users.noreply.github.com>
2024-03-29 23:24:17 +0530

    Update TestimonialCard.js
    stars remove from testimonial card

--- src/components/TestimonialCard/TestimonialCard.js
+++ src/components/TestimonialCard/TestimonialCard.js
@@ -6,7 +6,7 @@ import './TestimonialCard.css'
 const TestimonialCard = (props) => {
     return (
         <div className='testimonialCard'>
-            <div className="tCStars"><BiSolidStar /><BiSolidStar /><BiSolidStar /><BiSolidStar /><BiSolidStar /></div>
+            <div className="tCStars"></div>
             <div className="tCPara">{props.para}</div>
             <div className="tCClient">
                 <div className="tCCName">{props.cName}</div>
