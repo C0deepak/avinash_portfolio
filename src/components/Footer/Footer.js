eef1430794340b173677b69e40e9fb32eec21e60
Avinash Yadav <90188364+AceHere@users.noreply.github.com>
2024-06-17 22:24:08 +0530

    Update Footer.js
    remove footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,6 @@ const Footer = () => {
             copyright &copy; BeyondAvinash 2023-24
         </div>
         <div className="footerProm">
-            Designed by <a href="https://modernartist.netlify.app" target='_blank'>@modernArtist</a> & Developed by <a href="https://imalive.netlify.app" target='_blank'>@DBros.</a>
         </div>
     </div>
   )
