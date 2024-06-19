748597c82dcfbf6c70489188697dba021b4bf1ae
Avinash Yadav <90188364+AceHere@users.noreply.github.com>
2024-06-19 13:37:12 +0530

    Update Contact.js
    remove faqssss

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -50,18 +50,7 @@ const Contact = () => {
         </div>
       </div>
 
-      <div className='contactFaq'>
-        <div className='otherHeading'>Frequently Asked Questions <span>(FAQs)</span></div>
-
-        <div className="contactFaqWrap">
-          {
-            FaqData.map((ques) => {
-              return <AccordianCard key={ques.id} ques={ques.question} ans={ques.answer} />
-            })
-          }
-        </div>
-      </div>
-    </div>
+      
   )
 }
 
