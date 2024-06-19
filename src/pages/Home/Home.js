7f5d035bf77e0ec5553c631a9754382e74b1d356
Avinash Yadav <90188364+AceHere@users.noreply.github.com>
2024-06-19 13:35:32 +0530

    Update Home.js
    buk

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
 
                 <div className="homeHeroCardWrap">
                     <div className='homeHeroCardLevel'>
-                        <div className="homeHeroCard"><img src="/img/b1.png" alt="brand" /></div>
+                        <div className="homeHeroCard"><img src="/img/buk.png" alt="brand" /></div>
                         <div className="homeHeroCard"><img src="/img/beasly2.png" alt="brand" /></div>
                     </div>
                     <div className='homeHeroCardLevel'>
