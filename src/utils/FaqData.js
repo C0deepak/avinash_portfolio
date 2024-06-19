4e6d821076ac1544d8eb9e845b47758498188be8
Avinash Yadav <90188364+AceHere@users.noreply.github.com>
2024-06-19 13:19:15 +0530

    Update FaqData.js
    na rahega faq na bajegi

--- src/utils/FaqData.js
+++ src/utils/FaqData.js
@@ -1,19 +1,10 @@
 const FaqData = [
     {
-        id: 1,
-        question: 'How do I contact you for video editing services?',
-        answer: 'You can easily get in touch with me through the "Contact" section of my website. Simply fill out the contact form with your name, email, and a brief message describing your project and your requirements. I will promptly respond to your inquiry and discuss your project in more detail.'
-    },
+            },
     {
-        id: 2,
-        question: 'What types of video editing services do you offer?',
-        answer: 'I offer a wide range of video editing services to meet your needs.Anything from Youtube Videos to Reels to Corporate Videos and Ads. Some dream project? I am all ears, please reach out to discuss how I can assist you.'
-    },
+          },
     {
-        id: 3,
-        question: 'Can I see examples of your previous work?',
-        answer: 'Of course! You can view a selection of my previous video editing projects in the "WORKS" section of my website. Happy Scrolling. '
-    },
+            },
 ]
 
 export {FaqData};
